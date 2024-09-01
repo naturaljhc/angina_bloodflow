22ae18ab88df4a129ea558da4b9400229078bd07
Natural Chan <naturaljhc@gmail.com>
2024-08-31 22:19:45 -0700

    Updated folder structure with test files

---
+++ src/main.f90
@@ -0,0 +1,15 @@
+program test_fortran
+    implicit none
+    integer :: a, b, sum
+
+    ! Initialize variables
+    a = 5
+    b = 10
+
+    ! Perform a simple addition
+    sum = a + b
+
+    ! Print the result
+    print *, 'The sum of', a, 'and', b, 'is', sum
+
+end program test_fortran
